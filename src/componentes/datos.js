d26877e6622e8e4950ddc05cdb8cfbf40fa8d31e
Tomas <tomascf123@gmail.com>
2020-05-28 22:58:41 -0500

    Se arrega problema con la hora Ahora resta bien, y da bien la hora en horas colombianas

--- src/componentes/datos.js
+++ src/componentes/datos.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 
 export class Datos extends Component {
   render() {
+    const {fecha}= this.props
+    var hora = fecha.substring(11,13);
+    if(hora>=0 && hora <=5){
+      hora = 24+parseInt(hora);
+    }
+    hora= hora -5;
+    hora =  hora + fecha.substring(13,19);
+    //hora = hora-5;
     return (
       <div className="datos">
         <article className="message is-dark">
           <div className="message-headeruwu">
           <p>Fecha: {this.props.fecha.substring(0,10)}</p>
-          <p>Hora: {this.props.fecha.substring(11,19)}</p>
+          <p>Hora: {hora}</p>
           </div>
           <div className="message-body">
             <p>El tiempo fue de : {this.props.tiempo} s</p>
